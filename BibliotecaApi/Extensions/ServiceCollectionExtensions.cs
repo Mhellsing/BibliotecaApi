551cb4a46ea6b89fef0f7f3afb5647bd512513f3
Mhellsing <marcusmalkav@gmail.com>
2025-01-11 19:31:06 -0300

    Implementando LivroService

---
+++ BibliotecaApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BibliotecaApi.Services;
+using BibliotecaApi.Services.Interfaces;
+
+namespace BibliotecaApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ILivroService, LivroService>();
+
+            return services;
+        }
+    }
+}
