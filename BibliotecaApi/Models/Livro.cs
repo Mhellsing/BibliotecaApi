f4c5fa66fe870ae68f92161294a2ea23ee87ed55
Mhellsing <marcusmalkav@gmail.com>
2025-01-11 19:28:12 -0300

    Correcoes metodos LivroController, ajustes LivroResponse, ajuste LivroModel

--- BibliotecaApi/Models/Livro.cs
+++ BibliotecaApi/Models/Livro.cs
@@ -6,13 +6,13 @@ namespace BibliotecaApi.Models
     public class Livro : BaseModel
     {        
         public int NumeroPaginas { get; set; }
-        public int AutorId { get; set; }
-        public int EditoraId { get; set; }
-        public string Titulo { get; set; }
+        public string? Autor { get; set; }
+        public string? Editora { get; set; }
+        public string? Titulo { get; set; }
         public string? CapaUrl { get; set; }
-        public Idioma Idioma { get; set; }
-        public Categoria Categoria { get; set; }
-        public StatusLeitura StatusLeitura { get; set; }
+        public Idioma? Idioma { get; set; }
+        public Categoria? Categoria { get; set; }
+        public StatusLeitura? StatusLeitura { get; set; }
         public DateTime DataCadastro { get; set; }
     }
 }
